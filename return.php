038674134e3a01a50282b893a7708119ae28e6e5
alecio70 <alecioccb70@gmail.com>
2024-02-22 02:41:26 -0300

    Dev Notes API (structural): creating Insert

--- return.php
+++ return.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: *");
 header("Content-type: application/json");
 echo json_encode($array);
 exit;
